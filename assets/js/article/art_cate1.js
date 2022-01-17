4bb084d6035400b44596a0f1010728ab48ceb472
itcast123 <wz585688@163.com>
2022-01-17 21:03:20 +0800

    完成管理功能开发

--- assets/js/article/art_cate1.js
+++ assets/js/article/art_cate1.js
@@ -1,4 +1,5 @@
-$(function() {
+$(function () {
+ 
   var layer = layui.layer
   var form = layui.form
 
@@ -56,6 +57,7 @@ $(function() {
       title: '修改文章分类',
       content: $('#dialog-edit').html()
     })
+    
 
     var id = $(this).attr('data-id')
     // 发起请求获取对应分类的数据
@@ -67,16 +69,15 @@ $(function() {
       }
     })
   })
-
   // 通过代理的形式，为修改分类的表单绑定 submit 事件
-  $('body').on('submit', '#form-edit', function(e) {
+  $('body').on('submit', function (e) {
     e.preventDefault()
     $.ajax({
-      method: 'POST',
-      url: '/my/article/updatecate',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
+      method:'POST',
+      url:'/my/article/updatecate',
+      data:$(this).serialize(),
+      success: function (res) {
+        if(res.status !==0) {
           return layer.msg('更新分类数据失败！')
         }
         layer.msg('更新分类数据成功！')
@@ -85,24 +86,23 @@ $(function() {
       }
     })
   })
-
-  // 通过代理的形式，为删除按钮绑定点击事件
-  $('tbody').on('click', '.btn-delete', function() {
+  //通过代理的形式，为删除按钮绑定点击事件
+  $('tbody').on('click','.btn-delete',function () {
     var id = $(this).attr('data-id')
-    // 提示用户是否要删除
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
-      $.ajax({
-        method: 'GET',
-        url: '/my/article/deletecate/' + id,
-        success: function(res) {
-          if (res.status !== 0) {
-            return layer.msg('删除分类失败！')
-          }
-          layer.msg('删除分类成功！')
+    //提示用户是否删除
+    layer.confirm('确认删除？',{icon:3,title:'提示'}, function (index) {
+    $.ajax({
+      method:'GET',
+      url:'/my/article/deletecate' + id,
+      success:function (res) {
+        if(res.status !==0) {
+          return layer.msg('删除分类失败！')
+        }
+        layer.msg('删除分类成功！')
           layer.close(index)
           initArtCateList()
-        }
-      })
+      }
     })
-  })
+    })
+  }) 
 })
